2d82e19b247193f0d4c9896c40bc799987428711
UnmatchedBracket <103469241+UnmatchedBracket@users.noreply.github.com>
2024-09-21 22:34:48 -0600

    add NoDotForUnreads plugin

---
+++ src/plugins/noDotForUnreads/index.tsx
@@ -0,0 +1,23 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2023 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { Devs } from "@utils/constants";
+import definePlugin, { OptionType } from "@utils/types";
+
+export default definePlugin({
+    name: "NoDotForUnreads",
+    description: "Disables the red dot in the tray when you only have unreads.",
+    authors: [Devs.UnmatchedBracket],
+    patches: [
+        {
+            find: "SYSTEM_TRAY_OPEN_VOICE_SETTINGS",
+            replacement: {
+                match: /=[^,]+.hasAnyUnread\(\)/,
+                replace: "=false"
+            }
+        }
+    ]
+});
