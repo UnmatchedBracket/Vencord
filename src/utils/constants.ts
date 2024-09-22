02a8f58f476d03b827975c4f7e0c61c9d47b2c07
UnmatchedBracket <103469241+UnmatchedBracket@users.noreply.github.com>
2024-09-21 22:34:48 -0600

    add NoDotForUnreads plugin

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -598,6 +598,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
         name: "Cootshk",
         id: 921605971577548820n
     },
+    UnmatchedBracket: {
+        name: "UnmatchedBracket",
+        id: 971943551816581161n
+    },
 } satisfies Record<string, Dev>);
 
 // iife so #__PURE__ works correctly
